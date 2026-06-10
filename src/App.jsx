27f91c3e0d11870698d28e24adcbd73684a0e193
Andrii Avramenko <andreitvmm228@gmail.com>
2026-06-10 17:38:44 +0100

    New icon and build fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ import Filter from "./components/Filter/Filter";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -47,27 +42,20 @@ class App extends Component {
   };
 
   componentDidMount() {
-    console.log("Components did mount")
-
     const savedContacts = JSON.parse(localStorage.getItem("savedContacts"))
-    console.log(savedContacts)
 
     this.setState({
-      contacts: savedContacts,
+      contacts: savedContacts ?? [],
     })
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevState)
-
     const prevContacts = prevState.contacts
     const nextContacts = this.state.contacts
     
-    if (prevContacts !== nextContacts) {
-      console.log("succesfully updated!")
+    if (prevContacts === nextContacts) return
 
-      localStorage.setItem("savedContacts", JSON.stringify(nextContacts))
-    }
+    localStorage.setItem("savedContacts", JSON.stringify(nextContacts))
   }
 
   render() {
